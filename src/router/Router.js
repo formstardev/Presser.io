6326e99c474e3a83ed52b298e49b498dd65030f1
formstardev <abandonraj@gmail.com>
2023-05-29 19:15:42 -0700

    Auth pages completed

--- src/router/Router.js
+++ src/router/Router.js
@@ -8,6 +8,9 @@ import Landing from '../views/landing';
 import Splash from '../views/Auth/splash';
 import Register from '../views/Auth/register';
 import Veryfication from '../views/Auth/verification';
+import Login from '../views/Auth/login'
+import Confirmation from '../views/Auth/confirmation'
+
 
 const Routes = () => {
   return (
@@ -16,7 +19,9 @@ const Routes = () => {
         <Route path="/" component={ Splash } exact />
         <Route path="/landing" component = { Landing } exact />
         <Route path="/register" component = { Register } exact />
-        <Route path="/verification" component = { Veryfication } exact/> 
+        <Route path="/verification" component = { Veryfication } exact/>
+        <Route path="/login" component = { Login } exact/>
+        <Route path="/confirmation" component = { Confirmation } exact/>        
       </Switch>
     </BrowserRouter>
   );
--- src/views/Auth/login/index.js
+++ src/views/Auth/login/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Usericon from "../../../assets/img/Icon/user.png";
 import Emailicon from "../../../assets/img/Icon/email.png";
 import Passwordicon from "../../../assets/img/Icon/password.png";
 
 
-const Register = () => {
+const Login = () => {
     return (
         <div className="sm:bg-[#edeff1] bg-[#f4f4f4] min-h-screen justify-center flex">
             <div className="sm:block hidden relative sm:flex sm:flex-col sm:mt-0 sm:justify-center sm:items-center">
@@ -13,26 +12,18 @@ const Register = () => {
                     <figure className="sm:block" >
                         <img className="" src="/images/presser_final-logo.png" alt=""></img>
                     </figure> 
-                    <p className="text-[34px] mt-[30px] font-medium normal color-[#000] w-full">Create your Account</p>
-                    <div className="mt-[44px] ml-[22px] w-[287px] flex items-center border-b border-[#000] border-light py-2 mr-[22px] ">
-                        <p className="text-[18px] w-fit-content color-[#000] font-normal text-left">Full Name</p>
-                        <input className="appearance-none ml-[5px] mr-[10px] bg-transparent border-none text-gray-700 focus:outline-none" type="text" aria-label="FullName" placeholder="Full Name"></input>
-                    </div>
+                    <p className="text-[34px] mt-[30px] font-medium normal color-[#000] w-full">Login your Account</p>
                     <div className="mt-[26px] ml-[22px] flex items-center border-b border-[#000] border-light py-2 mr-[22px] ">
                         <p className="text-[18px] color-[#000] font-normal text-left">E-mail</p>
-                        <input className="appearance-none ml-[5px] mr-[10px] bg-transparent border-none text-gray-700 focus:outline-none" type="email" aria-label="Email" placeholder="Email Address"></input>
+                        <input className="appearance-none ml-[5px] mr-[10px] bg-transparent border-none text-gray-700 focus:outline-none" type="email" aria-label="Email" placeholder=""></input>
                     </div>
                     <div className="mt-[26px] ml-[22px] flex items-center border-b border-[#000] border-light py-2 mr-[22px] ">
                         <p className="text-[18px] color-[#000] font-normal text-left">Password</p>
-                        <input className="appearance-none ml-[5px] mr-[10px] bg-transparent border-none text-gray-700 focus:outline-none" type="password" aria-label="Password" placeholder="Password"></input>
-                    </div>
-                    <div className="mt-[26px] ml-[22px] flex items-center border-b border-[#000] border-light py-2 mr-[22px] ">
-                        <p className="text-[18px] sm:w-[146px] color-[#000] font-normal text-left">Confirm Password</p>
-                        <input className="appearance-none ml-[5px] mr-[10px] bg-transparent border-none text-gray-700 focus:outline-none mr-[22px]" type="password" aria-label="Password" placeholder="Confirm Password"></input>
-                    </div>
+                        <input className="appearance-none ml-[5px] mr-[10px] bg-transparent border-none text-gray-700 focus:outline-none" type="password" aria-label="Password" placeholder=""></input>
+                    </div>                    
                     <div className="mt-[61px] flex items-center justify-center">
                         <button className="flex-shrink-0 bg-[#3f85e3] hover:bg-teal-700 border-[#3f85e3] hover:border-teal-700 h-[35px] w-[131px] text-sm border-4 text-white py-1 px-2 rounded" type="button">
-                            Register
+                            Sign In
                         </button>
                     </div>
                 </div>
@@ -42,14 +33,9 @@ const Register = () => {
                     <img src="/images/blue.png" alt=""></img>
                 </figure>
                 <figure className="mt-[40px]">
-                    <img className="float-left" src="/images/Create your account.png" alt=""></img>
+                    <img className="float-left" src="/images/login-des.png" alt=""></img>
                 </figure>
-                <div className="mt-[44px] ml-[22px] w-[287px] flex items-center border-b border-[#5B59D6] border-light py-2 mr-[22px] ">
-                    <figure className="">
-                        <img src={Usericon} className=" text-[18px] color-[#000] font-normal focus:fill-[#5B59D6]" alt=""></img>    
-                    </figure>&nbsp;&nbsp;
-                    <input className="appearance-none mr-[10px] bg-transparent border-none text-gray-700 focus:outline-none mr-[22px]" type="text" aria-label="FullName" placeholder="Full Name"></input>
-                </div>
+
                 <div className="mt-[24px] ml-[22px] w-[287px] flex items-center border-b border-[#5B59D6] border-light py-2 mr-[22px] ">
                     <figure className="">
                         <img src={Emailicon} className=" text-[18px] color-[#000] font-normal focus:fill-[#5B59D6]" alt=""></img>    
@@ -61,23 +47,15 @@ const Register = () => {
                         <img src={Passwordicon} className=" text-[18px] color-[#000] font-normal focus:fill-[#5B59D6]" alt=""></img>    
                     </figure>&nbsp;&nbsp;
                     <input className="appearance-none mr-[10px] bg-transparent border-none text-gray-700 focus:outline-none mr-[22px]" type="text" placeholder="Password"></input>
-                </div>
-                <div className="mt-[24px] ml-[22px] w-[287px] flex items-center border-b border-[#5B59D6] border-light py-2 mr-[22px] ">
-                    <figure className="">
-                        <img src={Passwordicon} className=" text-[18px] color-[#000] font-normal focus:fill-[#5B59D6]" alt=""></img>    
-                    </figure>&nbsp;&nbsp;
-                    <input className="appearance-none mr-[10px] bg-transparent border-none text-gray-700 focus:outline-none mr-[22px]" type="text" aria-label="FullName" placeholder="Confirm Password"></input>
-                </div>
-                <div className="mt-[61px] flex items-center justify-center">
+                </div>                
+                <div className="mt-[201px] flex items-center justify-center">
                     <button className="flex-shrink-0 bg-[#3f85e3] hover:bg-teal-700 border-[#3f85e3] hover:border-teal-700 h-[52px] w-[327px] text-[16px] font-medium border-4 text-white py-1 px-2 rounded rounded-xl" type="button">
-                        Register
+                        Log In
                     </button>
                 </div>
                 <div className="mt-[61px] flex items-center justify-center">
-                    <p className="text-[14px] text-[#868686]">Do you have an account? &nbsp;
-                        <Link to="/login">
-                            <button className="text-[#3F85E3] font-bold">Login</button>
-                        </Link>
+                    <p className="text-[14px] text-[#868686]">Don't have an account? &nbsp;
+                        <Link to="/register"><button className="text-[#3F85E3] font-bold">Register</button></Link>
                     </p>
                 </div>
 
@@ -85,4 +63,4 @@ const Register = () => {
         </div>
     )
 }
-export default Register;
+export default Login;
