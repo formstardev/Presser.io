a3aafb10dc5f1a7b85a92ca0fbfff7c5552ea9cb
formstardev <abandonraj@gmail.com>
2023-06-08 21:20:32 -0700

    sidebar error fixed

--- src/component/home/sidebar.js
+++ src/component/home/sidebar.js
@@ -1,35 +1,19 @@
-/*
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ]
-  }
-  ```
-*/
 import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import {
-  HomeIcon,
-  XIcon,
+  HomeIcon
 } from '@heroicons/react/outline'
 import { FiBell, FiMail, FiSettings } from 'react-icons/fi'
 import { FaUser, FaUsers } from 'react-icons/fa'
+import { Link, useHistory } from 'react-router-dom';
 
-const navigation = [
-  { name: 'Home', href: '/home', icon: HomeIcon, current: true },
-  { name: 'Message', href: '/message', icon: FiMail, current: false },
-  { name: 'Groups', href: '/group', icon:FaUsers, current: false },
-  { name: 'Notifications', href: '#', icon: FiBell, current: false },
-  { name: 'Profile', href: '#', icon: FaUser, current: false },
-  { name: 'Settings', href: '#', icon: FiSettings, current: false },
+let navigation = [
+  { name: 'Home', to: '/home', icon: HomeIcon, current: true },
+  { name: 'Message', to: '/message', icon: FiMail, current: false },
+  { name: 'Groups', to: '/group', icon:FaUsers, current: false },
+  { name: 'Notifications', to: '#', icon: FiBell, current: false },
+  { name: 'Profile', to: '#', icon: FaUser, current: false },
+  { name: 'Settings', to: '#', icon: FiSettings, current: false },
 ]
 
 function classNames(...classes) {
@@ -38,6 +22,7 @@ function classNames(...classes) {
 
 export default function Sidebar() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
+  const history = useHistory();
 
   return (
     <div className="h-screen flex overflow-hidden bg-[#212529]">
@@ -49,81 +34,6 @@ export default function Sidebar() {
           open={sidebarOpen}
           onClose={setSidebarOpen}
         >
-          <Transition.Child
-            as={Fragment}
-            enter="transition-opacity ease-linear duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-          </Transition.Child>
-          <Transition.Child
-            as={Fragment}
-            enter="transition ease-in-out duration-300 transform"
-            enterFrom="-translate-x-full"
-            enterTo="translate-x-0"
-            leave="transition ease-in-out duration-300 transform"
-            leaveFrom="translate-x-0"
-            leaveTo="-translate-x-full"
-          >
-            <div className="relative flex-1 flex flex-col max-w-xs pt-5 pb-4 bg-gray-800">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-in-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in-out duration-300"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <div className="absolute top-0 right-0 mr-12 pt-2">
-                  <button
-                    className="ml-1 sm:hidden block flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                    onClick={() => setSidebarOpen(false)}
-                  >
-                    <span className="sr-only">Close sidebar</span>
-                    <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                  </button>
-                </div>
-              </Transition.Child>
-              <div className="flex-shrink-0 flex items-center px-4">
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                  alt="Workflow"
-                />
-              </div>
-              <div className="mt-5 flex-1 h-0 overflow-y-auto">
-                <nav className="px-2 space-y-1">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className={classNames(
-                        item.current ? 'bg-[#3F85E3] text-[#3F85E3]' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                        'group flex items-center px-2 py-2 text-base font-medium rounded-md'
-                      )}
-                    >
-                      <item.icon
-                        className={classNames(
-                          item.current ? 'text-[#3F85E3]' : 'text-gray-400 group-hover:text-gray-300',
-                          'mr-4 h-6 w-6'
-                        )}
-                        aria-hidden="true"
-                      />
-                      {item.name}
-                    </a>
-                  ))}
-                </nav>
-              </div>
-            </div>
-          </Transition.Child>
-          <div className="flex-shrink-0 w-14" aria-hidden="true">
-            {/* Dummy element to force sidebar to shrink to fit close icon */}
-          </div>
         </Dialog>
       </Transition.Root>
 
@@ -135,13 +45,24 @@ export default function Sidebar() {
             <div className="flex-1 flex flex-col overflow-y-auto">
               <nav className="flex-col mt-[37px] mx-[41px] bg-[#212529] space-y-4">
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
-                    href={item.href}
+                    to={item.to}
                     className={classNames(
                       item.current ? 'bg-[#3F85E3] bg-opacity-20 text-[#3F85E3]' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                       'group flex bg-[#373C40] items-center sm:w-[210px] px-2 py-2 text-[19px] font-bold rounded-md'
                     )}
+                    onClick={() => {
+                      const updatedNavigation = navigation.map((navItem) => {
+                        if (navItem.name === item.name) {
+                          return { ...navItem, current: true }
+                        } else {
+                          return { ...navItem, current: false }
+                        }
+                      })
+                      navigation = updatedNavigation
+                      history.push(item.to); // navigate to the specified link
+                    }}
                   >
                     <item.icon
                       className={classNames(
@@ -151,7 +72,7 @@ export default function Sidebar() {
                       aria-hidden="true"
                     />
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </nav>
               <div className='mt-4 ml-[34px] flex flex-col justify-center items-center'>
